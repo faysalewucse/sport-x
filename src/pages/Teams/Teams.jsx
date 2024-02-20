882d6b64e859842d66dc60dd1b7f273ed6c57bec
Faysal <faysal.ewucse@gmail.com>
2024-02-20 07:35:41 +0600

    add season dropdown to teams page

--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import TeamStates from "./TeamStates";
 import ListedPlayers from "./ListedPlayers";
 import { useGameContext } from "../../context/GameContext";
+import { Select } from "antd";
 
 ChartJS.register(
   CategoryScale,
@@ -25,7 +26,11 @@ ChartJS.register(
 const Teams = () => {
   const { games } = useGameContext();
 
-  const teamsData = games?.filter((game) => game.team === game.sp_name);
+  const [selectedSeason, setSelectedSeason] = useState(2024);
+
+  const teamsData = games?.filter(
+    (game) => game.team === game.sp_name && game.sea === selectedSeason
+  );
 
   const [selectedTeam, setSelectedTeam] = useState(
     localStorage.getItem("selectedTeam") || 0
@@ -40,7 +45,8 @@ const Teams = () => {
     },
     scales: {
       x: {
-        min: 40,
+        min: 0,
+        max: 120,
       },
     },
     responsive: true,
@@ -82,31 +88,50 @@ const Teams = () => {
     ],
   };
 
+  const handleSeason = (value) => {
+    setSelectedSeason(value);
+  };
+
   return (
     <div className="p-5">
       <div>
+        <h6 className="text-sm font-semibold mb-1">Select Season</h6>
+        <Select
+          className="mb-3"
+          defaultValue="2024"
+          style={{ width: 120 }}
+          onChange={handleSeason}
+          options={[
+            { value: 2024, label: "2024" },
+            { value: 2023, label: "2023" },
+          ]}
+        />
         <h1 className="text-xl font-bold">
           Teams : {teamsData[selectedTeam].team}
         </h1>
         <Team
+          teamsData={teamsData}
           category={"AL East"}
           setSelectedTeam={setSelectedTeam}
           startIndex={0}
           endIndex={5}
         />
         <Team
+          teamsData={teamsData}
           category={"AL Central"}
           setSelectedTeam={setSelectedTeam}
           startIndex={5}
           endIndex={10}
         />
         <Team
+          teamsData={teamsData}
           category={"AL West"}
           setSelectedTeam={setSelectedTeam}
           startIndex={10}
           endIndex={15}
         />
         <Team
+          teamsData={teamsData}
           category={"NL East"}
           setSelectedTeam={setSelectedTeam}
           startIndex={15}
@@ -114,12 +139,14 @@ const Teams = () => {
         />
 
         <Team
+          teamsData={teamsData}
           category={"NL Central"}
           setSelectedTeam={setSelectedTeam}
           startIndex={20}
           endIndex={25}
         />
         <Team
+          teamsData={teamsData}
           category={"NL West"}
           setSelectedTeam={setSelectedTeam}
           startIndex={25}
@@ -128,14 +155,14 @@ const Teams = () => {
       </div>
       {/* <Collapse size="large" accordion items={items} /> */}
       <Bar
-        title={teamsData[selectedTeam].gid}
+        title={teamsData[selectedTeam]?.gid}
         className="my-10"
         options={options}
         data={data}
       />
       <TeamStates teamsData={teamsData} selectedTeam={selectedTeam} />
       <ListedPlayers
-        selectedTeam={teamsData[selectedTeam].team}
+        selectedTeam={teamsData[selectedTeam]?.team}
         lastNames={teamsData[selectedTeam].y_arr.split(",")}
       />
     </div>
@@ -144,13 +171,13 @@ const Teams = () => {
 
 export default Teams;
 
-const Team = ({ category, setSelectedTeam, startIndex, endIndex }) => {
-  const { games } = useGameContext();
-
-  console.log(games);
-
-  const teamsData = games?.filter((game) => game.team === game.sp_name);
-
+const Team = ({
+  teamsData,
+  category,
+  setSelectedTeam,
+  startIndex,
+  endIndex,
+}) => {
   return (
     <p className="font-semibold text-lg flex gap-2">
       <p className="">{category} : </p>
